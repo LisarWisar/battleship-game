5d9b1422d735412a9063b78416e1709d7d2234ae
LisarWisar <vsoto.zoro@gmail.com>
2024-06-23 21:04:08 -0400

    Added board layout

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,45 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+
+	let userTiles = [];
+	const sideCoordinates = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
+	for (let i = 0; i<11; i++){
+		let tempArray = [];
+		for (let j = 0; j<11; j++){
+			if ((i == 0 || i == 10) && (j == 0 || j == 10)){
+				tempArray.push({"posClass": "cornerTile", "xCoordinate": i, "yCoordinate": j, "tileText": ""});
+			}
+			else if((i == 0 || i == 10)){
+				if (j == 1){
+					tempArray.push({"posClass": "firstTopTile", "xCoordinate": i, "yCoordinate": j, "tileText": j});
+				}
+				else {
+					tempArray.push({"posClass": "topTile", "xCoordinate": i, "yCoordinate": j, "tileText": j});
+				}
+			}
+			else if(j == 0 || j == 10){
+				if (i == 1){
+					tempArray.push({"posClass": "firstSideTile", "xCoordinate": i, "yCoordinate": j, "tileText": sideCoordinates[i-1]});
+				}
+				else{
+					tempArray.push({"posClass": "sideTile", "xCoordinate": i, "yCoordinate": j, "tileText": sideCoordinates[i-1]});
+				}
+			}
+			else {
+				tempArray.push({"posClass": "centerTile", "xCoordinate": i, "yCoordinate": j, "tileText": ""});
+			}
+		}
+		userTiles.push(tempArray);
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="gameBoard">
+			{userTiles.map(boardColumn => (
+				boardColumn.map(tile => (
+					<div className={tile.posClass}>{tile.tileText}</div>
+				))
+			))}
 		</div>
 	);
 };
